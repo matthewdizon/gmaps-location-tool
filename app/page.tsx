6b85d8473682a44cf0b6496eb6bb5154f5a91d28
matthewdizon <dominic.dizon@obf.ateneo.edu>
2023-09-17 09:42:21 +1200

    added responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   };
 
   return (
-    <main className="p-12">
+    <main className="p-12 grid">
       <div className="flex gap-2 flex-col pb-6 w-1/2">
         <h1 className="text-3xl font-bold">Google Maps Location Tool</h1>
         <p>
@@ -30,47 +30,49 @@ export default function Home() {
       <h2 className="text-2xl font-bold py-2">
         Origin: {origin.split("+").join(" ") || "None"}
       </h2>
-      <div className="flex gap-2 pb-4">
-        <button
-          className="p-2 bg-gray-500 rounded hover:bg-gray-50 hover:text-black"
-          onClick={() => setNumberOfMaps(numberOfMaps + 1)}
-        >
-          Add Map
-        </button>
-        <button
-          className="p-2 bg-gray-500 rounded hover:bg-gray-50 hover:text-black"
-          onClick={() => setNumberOfMaps(numberOfMaps - 1)}
-        >
-          Remove Map
-        </button>
-      </div>
-      <div className="pb-6 flex gap-2">
-        <input
-          type="text"
-          placeholder="Origin"
-          value={origin.split("+").join(" ")}
-          className="rounded text-black"
-          onChange={(e) => setOrigin(convertText(e.target.value))}
-        />
-        <div className="flex gap-2">
-          {modes.map((travelMode, index) => {
-            return (
-              <button
-                onClick={() => setMode(travelMode)}
-                key={index}
-                className={`p-2 rounded ${
-                  mode === travelMode
-                    ? "bg-gray-50 text-black"
-                    : "bg-gray-500 hover:bg-gray-50 hover:text-black"
-                }`}
-              >
-                {travelMode.toUpperCase()}
-              </button>
-            );
-          })}
+      <div className="flex flex-col md:flex-row">
+        <div className="flex gap-2 pb-4">
+          <button
+            className="p-2 bg-gray-500 rounded hover:bg-gray-50 hover:text-black"
+            onClick={() => setNumberOfMaps(numberOfMaps + 1)}
+          >
+            Add Map
+          </button>
+          <button
+            className="p-2 bg-gray-500 rounded hover:bg-gray-50 hover:text-black"
+            onClick={() => setNumberOfMaps(numberOfMaps - 1)}
+          >
+            Remove Map
+          </button>
+        </div>
+        <div className="pb-6 flex gap-2">
+          <input
+            type="text"
+            placeholder="Origin"
+            value={origin.split("+").join(" ")}
+            className="rounded text-black"
+            onChange={(e) => setOrigin(convertText(e.target.value))}
+          />
+          <div className="flex gap-2">
+            {modes.map((travelMode, index) => {
+              return (
+                <button
+                  onClick={() => setMode(travelMode)}
+                  key={index}
+                  className={`p-2 rounded ${
+                    mode === travelMode
+                      ? "bg-gray-50 text-black"
+                      : "bg-gray-500 hover:bg-gray-50 hover:text-black"
+                  }`}
+                >
+                  {travelMode.toUpperCase()}
+                </button>
+              );
+            })}
+          </div>
         </div>
       </div>
-      <div className="grid grid-cols-2 gap-4">
+      <div className="grid md:grid-cols-2 gap-4">
         {Array.from({ length: numberOfMaps }, (_, i) => i + 1).map((id) => {
           return (
             <Map
